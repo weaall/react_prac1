87a83dbadd06fba9d179153ea7da327521037432
weaall <weaall88@gmail.com>
2023-09-07 12:38:03 +0900

    print info

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import axios from "axios";
 import React from 'react';
 
 
-class App extends React.Component{
+class App extends React.Component {
+  state = { arr: [] }
 
-  state = {
-    arr:[]
+  setData = async () => {
+    let user = await axios.get("/member_table");
+    user = user.data;
+    this.setState({ arr: user });
+  }
+
+  componentDidMount() {
+    this.setData();
   }
 
   selectAll = async () => {
@@ -14,24 +21,29 @@ class App extends React.Component{
     result = result.data;
     console.log(JSON.stringify(result));
     alert(JSON.stringify(result));
-  };
+  }
 
   callName = async () => {
     let name = await axios.get("/member_table/name");
     name = name.data;
     console.log(JSON.stringify(name));
     alert(JSON.stringify(name));
-  };
+  }
 
-  render(){
+  render() {
 
-  return (
-    <div id="App">
-      <button onClick={this.selectAll}>전체 조회</button>
-      <button onClick={this.callName}>이름 조회</button>
-      <h3>{}</h3>
-    </div>
-  );
+    return (
+      <div id="App">
+        <button onClick={this.selectAll}>전체 조회</button>
+        <button onClick={this.callName}>이름 조회</button>
+        
+        <div>
+          {this.state.arr.map((myMap) => {
+            return <p>{myMap.id} {myMap.name} {myMap.address} {myMap.name}</p>
+          })}
+        </div>
+      </div>
+    );
   }
 }
 
